7f28f7c4cf940d3c7b5a08d6452653404866696b
JS-Project96 <jshorter206854@gmail.com>
2023-07-10 11:13:00 +0100

    Created computer choice function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 // get computers choice
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const choice = Math.floor(Math.random() * 3);
+    return choices[choice]
+};
 // get users choice
 // compare computer and users choice
 // declare the result
