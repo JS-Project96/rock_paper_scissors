03d4063257d9b1373545a4e8e06de8e31725cec4
JS-Project96 <jshorter206854@gmail.com>
2023-07-10 11:30:42 +0100

    Created user choice function with correct choice logic

--- js/script.js
+++ js/script.js
@@ -2,9 +2,20 @@
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const choice = Math.floor(Math.random() * 3);
-    return choices[choice]
+    return choices[choice];
 };
+
 // get users choice
+function getUsersChoice() {
+    let choice = prompt("Enter rock, paper or scissors: ").toLowerCase(); // to lower case makes comparison easier
+    while (true) {
+        if (choice === "rock" || choice === "paper" || choice === "scissors"){
+            return choice;
+        } else {
+            choice = prompt("Please enter a valid input. rock, paper or scissors: ").toLowerCase();
+        };
+    };
+};
 // compare computer and users choice
 // declare the result
 // ask user if they would like to play again
